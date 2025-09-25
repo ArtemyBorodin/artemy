3765a651f9159170735a2d02617b36ff0683880b
Artemy <art_borodin@mail.ru>
2025-09-25 18:45:30 +0300

    классная работа

---
+++ classwork20.09.py
@@ -0,0 +1,18 @@
+#0. Напишите программу, которая создает список из чисел от 1 до 20 и выводит его.
+#list = []
+#for x in range(1, 21):
+    #list.append(x)
+#print(list)
+from itertools import product
+
+#0. Напишите программу, которая находит сумму и произведение всех элементов в списке.
+#list = [1,2,3,4,5,-5,0,12,-1]
+#summ = 0
+#product = 1
+#for x in list:
+#    summ += x
+#    product *= x
+#print(summ)
+#print(product)
+
+
